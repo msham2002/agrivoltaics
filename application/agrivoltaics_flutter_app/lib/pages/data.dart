0435fe302909d28e3cd9897fe37b1f2e3bb337ff
Will Hopkins <willhopkins44@gmail.com>
2023-02-15 19:53:45 -0500

    cleaned code

--- application/agrivoltaics_flutter_app/lib/pages/data.dart
+++ application/agrivoltaics_flutter_app/lib/pages/data.dart
@@ -1,8 +1,5 @@
-// import 'package:easy_web_view/easy_web_view.dart' as EasyWebView;
 import 'package:flutter/material.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:io' show Platform;
-
+// import 'dart:io' show Platform;
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class DataDashboard extends StatefulWidget {
@@ -13,45 +10,27 @@ class DataDashboard extends StatefulWidget {
 }
 
 class _DataDashboardState extends State<DataDashboard> {
-  // late final WebViewController _controller;
-  String embedPath = '<iframe src="https://10.0.2.2:3000/dashboard-solo/new?gettingstarted&orgId=1&panelId=2" width=100% height=100% frameborder="0"></iframe>';
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   controller = WebViewController()..loadRequest(Uri('google.com'))
-  // }
+  // localhost = 10.0.2.2 on Android (don't need to check for iOS since we are using localhost for dev purposes)
+  // width=100%, height=100%
+  String embedPath =
+    '<iframe src="https://10.0.2.2:3000/d-solo/iTY2S2JVz/new-dashboard?orgId=1&theme=dark&panelId=2" width=100% height=100% frameborder="0"></iframe>';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-
-          // DO NORMAL WEBVIEW FOR IOS AND ANDROID
-
-          // child: (Platform.isIOS || Platform.isAndroid)
-          //     ? WebView(
-          //         initialUrl: Uri.dataFromString(embedPath, mimeType: 'text/html').toString(),
-          //         javascriptMode: JavascriptMode.unrestricted,
-          //         onWebViewCreated: (WebViewController controller) {
-          //           _controller = controller;
-          //         })
-          //     : EasyWebView.EasyWebView(
-          //         src:
-          //             'https://play.grafana.org/d/000000016/1-time-series-graphs?orgId=1&viewPanel=1')),
+      body: OrientationBuilder(builder: (context, orientation) {
+        return Center(
+          // TODO: Log into Grafana via headless webview, then persist session into headful webview
 
           child: InAppWebView(
-            initialUrlRequest: URLRequest(
-              url: Uri.dataFromString(embedPath, mimeType: 'text/html')
-            ),
+            initialUrlRequest: URLRequest(url: Uri.dataFromString(embedPath, mimeType: 'text/html')),
             onReceivedServerTrustAuthRequest: (controller, challenge) async {
-              print(challenge);
               return ServerTrustAuthResponse(action: ServerTrustAuthResponseAction.PROCEED);
             },
           )
-
-
-      )
+        );
+      })
     );
   }
 }
