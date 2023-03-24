e6ef117ada1a9d0a73b0f4c1452ce7b0752f1c2e
Will Hopkins <willhopkins44@gmail.com>
2023-03-24 18:38:20 -0400

    WIP notification bell w/ drawer

--- application/agrivoltaics_flutter_app/lib/pages/home.dart
+++ application/agrivoltaics_flutter_app/lib/pages/home.dart
@@ -7,7 +7,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: const [
+          NotificationButton()
+        ],
+      ),
+      endDrawer: const Drawer(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -23,17 +28,54 @@ class HomePage extends StatelessWidget {
                   )
               },
             ),
-            ElevatedButton(
-              child: const Text('Manage Sensors'),
-              onPressed: () => {print('hello')},
-            ),
+            // TODO: determine whether we will have this functionality
+            // ElevatedButton(
+            //   child: const Text('Manage Sensors'),
+            //   onPressed: () => {print('hello')},
+            // ),
             ElevatedButton(
               child: const Text('Settings'),
-              onPressed: () => {print('hello')},
+              onPressed: () => {debugPrint('Settings selected')},
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class NotificationButton extends StatelessWidget {
+  const NotificationButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+            onPressed: () {
+              Scaffold.of(context).openEndDrawer();
+            },
+            icon: Stack(
+              children: const [
+                Icon(
+                  Icons.notifications,
+                ),
+                Positioned(
+                  child: Icon(
+                    Icons.brightness_1,
+                    color: Colors.red,
+                    size: 9.0
+                  )
+                )
+              ]
+            )
+          ),
+        );
+      }
     );
   }
 }
