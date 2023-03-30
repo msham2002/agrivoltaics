62e140d3a0d27d66c2a9c9f634c417622a0a4b04
martinyg <45515274+martinyg@users.noreply.github.com>
2023-03-30 17:11:14 -0400

    added micro scervice

--- application/microservices/py-weather-microservice/requestcall.py
+++ application/microservices/py-weather-microservice/requestcall.py
@@ -1,20 +1,71 @@
 import requests
 import json
 
-response = requests.get("https://api.weather.gov/points/39.260375,-84.624175")
+#lat = "39.260375"
+#long = "-84.624175"
 
-print(response)
+lat = "36.269035"
+long = "-103.649067"
 
-print("Pull successful")
+#36.269035, -103.649067
 
-print(response.json())
+response = requests.get(f"https://api.weather.gov/points/{lat},{long}")
 
-print("pull over")
+if (response != None):
+    print("Pull successful")
+else:
+    print("Issue with pull")
+    
+json_data = response.json()
 
-encoded_weatherpull = json.dumps(response.json())
-decoded_weatherpull = json.loads(encoded_weatherpull)
+alert_api = json_data["properties"]["forecastGridData"]
 
+print("Printing the key")
 
-for key in encoded_weatherpull:
-    if(encoded_weatherpull[key] == 'forecastGridData'):
-        print(key)
+print(alert_api)
+
+hazard_call = requests.get(alert_api)
+
+if(hazard_call != None):
+    print("Pull successful")
+else:
+    print("Issue with pull")
+    
+hazard_data = hazard_call.json()
+
+hazard_key = hazard_data["properties"]["hazards"]
+
+print("printing hazards")
+
+print(hazard_key)
+
+#need to call for the past 12 hours 
+#array of dictionaries
+#we want to give them phenomen array that has the time and the siginificance
+#ideal structure to send
+#lifetime : 12 hours
+#only care about past 12 hours
+[
+    {
+            "id": "340983409",
+            "time": "2023-03-30T17:00:00+00:00/PT9H",
+            "phenomenom": "Fire weather",
+            "significance": "Watch"
+    },
+    {
+            
+            "id": "8594080980",
+            "time": "2023-03-40T17:00:00+00:00/PT9H",
+            "phenomenom": "Dense Smoke",
+            "significance": "Advisory"
+    }
+]
+
+hazard_vals = hazard_key["values"]
+
+for instantTime in hazard_vals:
+    print(instantTime["validTime"])
+    for val in instantTime["value"]:
+            print(val["phenomenon"])
+            print(val["significance"])
+            
