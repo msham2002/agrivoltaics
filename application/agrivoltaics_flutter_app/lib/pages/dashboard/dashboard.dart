93c76bea6f1a488e7cfce05c10dbfca3c050d5f0
Will Hopkins <willhopkins44@gmail.com>
2023-04-02 20:18:02 -0400

    truncating milliseconds from dashboard date axis

--- application/agrivoltaics_flutter_app/lib/pages/dashboard/dashboard.dart
+++ application/agrivoltaics_flutter_app/lib/pages/dashboard/dashboard.dart
@@ -117,9 +117,9 @@ class _DashboardGraphState extends State<DashboardGraph> {
                   )
                 ),
                 primaryXAxis: CategoryAxis(),
-                series: <LineSeries<InfluxDatapoint, DateTime>>[
+                series: <LineSeries<InfluxDatapoint, String>>[
                   for (var data in snapshot.data!.entries)...[
-                    LineSeries<InfluxDatapoint, DateTime>(
+                    LineSeries<InfluxDatapoint, String>(
                       dataSource: InfluxData(data.value, appState.timezone).data,
                       xValueMapper: (InfluxDatapoint d, _) => d.timeStamp,
                       yValueMapper: (InfluxDatapoint d, _) => d.value,
