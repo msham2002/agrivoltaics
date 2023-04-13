d976ce24ed84e3016ba9c9c9de62bc695edf2e7e
Will Hopkins <willhopkins44@gmail.com>
2023-04-13 12:47:23 -0400

    removed unnecessary import

--- application/agrivoltaics_flutter_app/lib/app_state.dart
+++ application/agrivoltaics_flutter_app/lib/app_state.dart
@@ -1,6 +1,5 @@
 import 'package:agrivoltaics_flutter_app/app_constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:timezone/timezone.dart' as tz;
 
 class AppState extends ChangeNotifier {
