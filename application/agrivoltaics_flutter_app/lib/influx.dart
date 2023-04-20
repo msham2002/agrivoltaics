6283f27db7a65742761fe34e1b153ec930698706
Will Hopkins <willhopkins44@gmail.com>
2023-04-20 12:27:07 -0400

    no longer filling influxdb null values with averaged dummy data

--- application/agrivoltaics_flutter_app/lib/influx.dart
+++ application/agrivoltaics_flutter_app/lib/influx.dart
@@ -38,7 +38,7 @@ String _generateQuery
   |> filter(fn: (r) => r["_measurement"] == "Site ${site}")
   |> filter(fn: (r) => ${zoneQuery})
   |> filter(fn: (r) => ${fieldQuery})
-  |> aggregateWindow(every: ${timeInterval.value}${timeInterval.unit.fluxQuery}, fn: mean, createEmpty: false)
+  |> aggregateWindow(every: ${timeInterval.value}${timeInterval.unit.fluxQuery}, fn: mean, createEmpty: true)
   |> yield(name: "mean")
   ''';
 }
